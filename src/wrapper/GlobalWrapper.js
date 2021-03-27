94326ca3cd748227bb68e6de227702b6953c4edd
budlebee <budlebeee@gmail.com>
2021-03-27 11:53:45 +0900

    done: footer를 하단에 고정

--- src/wrapper/GlobalWrapper.js
+++ src/wrapper/GlobalWrapper.js
@@ -4,10 +4,17 @@ import { Footer } from "../components/Footer";
 export const GlobalWrapper = ({ children }) => {
   return (
     <>
-      <Nav />
-
-      {children}
-      <Footer />
+      <div
+        style={{
+          display: "flex",
+          minHeight: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <Nav />
+        <main style={{ flex: 1 }}>{children}</main>
+        <Footer />
+      </div>
     </>
   );
 };
